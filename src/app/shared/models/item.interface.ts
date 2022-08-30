7b06d9cc0eb8889f50c7d85f7c14577fa1c88260
skerrey <sethkerrey@gmail.com>
2022-08-30 18:09:41 -0500

    Create item interface

---
+++ src/app/shared/models/item.interface.ts
@@ -0,0 +1,15 @@
+/**
+============================================
+; Title: nodebucket
+; Author: Professor Krasso
+; Date: 21 August 2022
+; Modified By: Seth Kerrey
+; Description: nodebucket item interface
+; Code Attribution: Additional code from buwebdev
+;===========================================
+*/
+
+export interface Item {
+  _id: string;
+  text: string;
+}
