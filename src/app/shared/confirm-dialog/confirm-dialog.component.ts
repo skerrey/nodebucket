2bb192d865a4b0cbc9b2c805f77497de1484ae01
skerrey <sethkerrey@gmail.com>
2022-09-01 17:34:43 -0500

    Create confirm-dialog component

---
+++ src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,32 @@
+/**
+============================================
+; Title: nodebucket
+; Author: Professor Krasso
+; Date: 21 August 2022
+; Modified By: Seth Kerrey
+; Description: nodebucket confirm-dialog component ts
+; Code Attribution: Additional code from buwebdev
+;===========================================
+*/
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../models/dialog-data.interface';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  dialogData: DialogData;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.dialogData = data;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
