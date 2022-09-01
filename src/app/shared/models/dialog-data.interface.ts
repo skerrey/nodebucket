c9e8d911c47f55d7eaaca8d09b6ac16716b06a25
skerrey <sethkerrey@gmail.com>
2022-09-01 17:35:00 -0500

    Create dialog-data.interface.ts

---
+++ src/app/shared/models/dialog-data.interface.ts
@@ -0,0 +1,17 @@
+/**
+============================================
+; Title: nodebucket
+; Author: Professor Krasso
+; Date: 21 August 2022
+; Modified By: Seth Kerrey
+; Description: nodebucket dialog-data interface
+; Code Attribution: Additional code from buwebdev
+;===========================================
+*/
+
+export interface DialogData {
+  header: string;
+  body: string;
+}
+
+
